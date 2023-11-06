e16ebbf40210e3ff3352b3b773c1f1527530c3db
Simeon Meerson <55815704+simeonmeerson@users.noreply.github.com>
2023-11-06 18:03:43 -0500

    Update patternExtraction.scala
    Cleaning up comments

--- patternExtraction.scala
+++ patternExtraction.scala
@@ -9,88 +9,64 @@ object patternExtraction{
 	def main(args:Array[String]) = {
 		val conf = new SparkConf().setMaster("spark://Simeons-MacBook-Pro.local:7077").setAppName("patternExtraction")
 		val sc = new SparkContext(conf)
-
-				//.setMaster("spark://Simeons-Macbook-Pro.local:7077")
-
-		val txtOne = sc.textFile("/Users/simeonmeerson/programming/spark_disk/project/one.txt")  // RDD text One
-		val txtTwo = sc.textFile("/users/simeonmeerson/programming/spark_disk/project/two.txt")	 // RDD text Two
+		//.setMaster("spark://Simeons-Macbook-Pro.local:7077")
+		val txtOne = sc.textFile("/Users/.../spark_disk/project/one.txt")  // RDD text One
+		val txtTwo = sc.textFile("/users/.../programming/spark_disk/project/two.txt")	 // RDD text Two
 
 
 		println("RDD containing Union (contents of both files)")
 		val txtThree = txtOne.union(txtTwo)
 
-
-
 		println("Printing Contents of Union // (both files)")
 		//txtThree.collect.foreach(println)
-
 		println("----------------------------------------------------------------------------------------")
-
-		/*****************************************************************************************************
-						PROVIDED BY SLIDES - LECTURE 6 		
+		/*****************************************************************************************************	
 						Regular Expression to extract	BIRTHDAY Dates (Born....) 
-					ex:
-						(born June 14, 1946)										
+						ex: (born June 14, 1946)										
 		*****************************************************************************************************/
-
-
-		val dobRegEx = "(\\(born [^)]+\\))".r                     		      	// regular expression string = "(born ... anything up to )"
+		val dobRegEx = "(\\(born [^)]+\\))".r                     			
+		// regular expression string = "(born ... anything up to )"
 		val dob = txtThree.flatMap(line=>(dobRegEx findAllIn line))
-	
 		println("----------------------------------------------------------------------------------------")
-	
 		println("Printing DOB for each from each candidate/file")
 		println("	")			// spaces
 		println("	")			// spaces
-		//	TEMPORARILY REMOVING FOR SCREENSHOTS		dob.collect.foreach(println)	
-
-
+		// TEMPORARILY REMOVED	dob.collect.foreach(println)	
 		println("----------------------------------------------------------------------------------------")
 
 
-        /**************************************************************************************************** 		
-				YEARS
-
-        		Copied syntax from slides / example above 
-        		& Regular Expressions syntax @ ---> 
+      		/**************************************************************************************************** 		
+			YEARS: Regular Expressions syntax @ ---> 
          		http://www.tutorialspoint.com/scala/scala_regular_expressions.htm							
-         *****************************************************************************************************/
+         	*****************************************************************************************************/
 
         val years = "(\\d{4})".r									// RegEx to extract 4 consecutive digits
         val yearsExtracted 	= txtThree.flatMap(line=>(years findAllIn line)).map(word=>(word, 1))
         val yearsReduced 	= yearsExtracted.reduceByKey(_+_)
         println("Printing out Key, Value pairs of Years (4 consecutive digits)")
-	// 		TEMPORARILY REMOVING FOR SCREENSHOTS	        yearsReduced.collect.foreach(println)
+	// TEMPORARILY REMOVED	        yearsReduced.collect.foreach(println)
 	println("----------------------------------------------------------------------------------------")	
-
-
 	val yearsCount = yearsExtracted.count()
 	println("	")
 	println("	")
 	println("Number of times a year (four consecutive digits) had been extracted:")
 	println(yearsCount)
 	println("	")
-	println("	")a
-
+	println("	")
 	println("----------------------------------------------------------------------------------------")
 
-
         /**************************************************************************************************
-				DATES:		month = (January, February, March, April, May, June, July, August, October, November, December)
-						dd	= 	numbers (01-31)
-						year = Four digits
+	DATES:		month = (January, February, March, April, May, June, July, August, October, November, December)
+				dd	= 	numbers (01-31)
+				year = Four digits
 
-				Typically appears in month, dd, year  
+	Typically appears in month, dd, year  
 											
 	**************************************************************************************************/
-
-
-
 		val dates = "(([Jj]an(uary)*|[Ff]eb(ruary)*|[Mm]ar(ch)*|[Aa]pr(il)*|[Mm]ay|[Jj]un(e)*|[Jj]ul(y)*|[Aa]ug(ust)*|[Ss]ep(tember)*|[Oo]ct(ober)*|[Nn]ov(ember)*|[Dd]ec(ember)*)\\s+\\d{2}[\\s,]+\\d{4})".r
 		val datesExtracted = txtThree.flatMap(line=>(dates findAllIn line))
-
 		println("Dates Extracted from unionized RDD:")
-		// TEMPORARILY COMMENTING OUT FOR SCREENSHOTS datesExtracted.collect.foreach(println)
+		// TEMPORARILY REMOVED datesExtracted.collect.foreach(println)
 
 		println("----------------------------------------------------------------------------------------")
 		println("	")
@@ -101,15 +77,11 @@ object patternExtraction{
 		println("	")
 		println("----------------------------------------------------------------------------------------")
 
-
-
         /***************************************************************************************************
-
 			Quotes:
 			ex:
 				“ ...the agreement did not "compel the Trump organization to accept persons on welfare as 
 						tenants unless as qualified as any other tenant.”	”
-
 	***************************************************************************************************/
 
 		println("----------------------------------------------------------------------------------------")
@@ -117,8 +89,7 @@ object patternExtraction{
 		val quotes = "\"([^\"]*)\"".r
 		val quotesExtracted = txtThree.flatMap(line=>(quotes findAllIn line))
 		println("Printing contents in Quotes for Unionized RDD")
-	//		TEMPORARILY REMOVING FOR SCREENSHOTS!		quotesExtracted.collect.foreach(println)
-
+		// TEMPORARILY REMOVED		quotesExtracted.collect.foreach(println)
 
 		println("----------------------------------------------------------------------------------------")
 		println("		")
